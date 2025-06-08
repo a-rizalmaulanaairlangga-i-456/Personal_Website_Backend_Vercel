8eb4483cb55831fa470638abb0146036b251aa09
Rizal <rizalmaulanaairlanggad4itb2024@gmail.com>
2025-06-08 23:48:31 +0700

    Update title, favicon, and components

--- api/db_aca_achiev.js
+++ api/db_aca_achiev.js
@@ -1,6 +1,16 @@
 import { notion } from '../notionClient.js';
 
 export default async function handler(req, res) {
+  // Tambahkan header CORS
+  res.setHeader('Access-Control-Allow-Origin', '*'); // atau ganti '*' dengan 'http://localhost:3000'
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Tangani preflight request (OPTIONS)
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'GET') {
     return res.status(405).send('Method Not Allowed');
   }
