9366e28af1a94f6b3176c42d4782f261fc49d82d
Rizal <rizalmaulanaairlanggad4itb2024@gmail.com>
2025-06-08 23:18:27 +0700

    Update title, favicon, and components

--- api/db_collab_web_project.js
+++ api/db_collab_web_project.js
@@ -1,11 +1,15 @@
 import { notion } from '../notionClient.js';
 
-export default async function dbCollabWebProjectHandler(req, res) {
+export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).send('Method Not Allowed');
+  }
+
   try {
     const resp = await notion.databases.query({
       database_id: process.env.DB_COLLAB_WEBPROJECT_ID
     });
-    res.json(resp.results);
+    res.status(200).json(resp.results);
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
