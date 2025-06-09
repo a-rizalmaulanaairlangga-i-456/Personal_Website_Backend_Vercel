e3525c7a8305a2456a5af2ea1df13de272ed69cb
Rizal <rizalmaulanaairlanggad4itb2024@gmail.com>
2025-06-09 22:36:54 +0700

    Update url yang bisa akses

--- api/db_tools.js
+++ api/db_tools.js
@@ -1,10 +1,19 @@
 import { notion } from '../notionClient.js';
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://rizalmaulanaairlangga-personalwebsite.vercel.app'
+];
+
 export default async function handler(req, res) {
-  // Tambahkan header CORS
-res.setHeader('Access-Control-Allow-Origin', 'https://rizalmaulanaairlangga-personalwebsite.vercel.app/');
-res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   // Tangani preflight request (OPTIONS)
 if (req.method === 'OPTIONS') {
